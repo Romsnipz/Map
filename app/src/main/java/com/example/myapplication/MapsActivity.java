1ee411fcad180a3a02c1b9bae98ca8258ca3502c
romar <romarioprime@mail.ru>
2020-06-07 13:54:09 +0300

    Карта с зумом на Москву

--- app/src/main/java/com/example/myapplication/MapsActivity.java
+++ app/src/main/java/com/example/myapplication/MapsActivity.java
@@ -4,10 +4,12 @@ import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -28,10 +30,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        double lat = 55.751737;
+        double lng = 37.617337;
+        LatLng moscow = new LatLng(lat, lng);
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(55.751737, 37.617337);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Moscow"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(moscow).zoom(10).build();
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition);
+        mMap.animateCamera(cameraUpdate);
+        mMap.addMarker(new MarkerOptions().position(moscow).title("Marker in Moscow"));
     }
 }
